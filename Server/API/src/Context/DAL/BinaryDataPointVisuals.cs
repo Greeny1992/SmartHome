4a41be6c4e44385012df4dedffac5493dd2e53df
Georg Prassl <prassl@rocketsholding.com>
2022-03-06 10:31:36 +0100

    Init of exercise

---
+++ Server/API/src/Context/DAL/BinaryDataPointVisuals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Context.DAL
+{
+    public class BinaryDataPointVisuals : DataPointVisual
+    {
+        public BinaryDataPointVisuals() { }
+
+
+        public List<BinaryValueMapping> ValueMapping { get; set; } = new();
+    }
+
+    public class BinaryValueMapping
+    {
+        public Object Value { get; set; }
+
+        public String Text { get; set; }
+    }
+}
---
+++ Server/API/src/Context/DAL/DataSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Context.DAL
+{
+    public class DataSource
+    {
+
+        public DataSource() { }
+
+        public String Name { get; set; }
+
+        public Boolean Active { get; set; }
+
+    }
+}
